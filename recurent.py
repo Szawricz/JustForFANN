b662422e0409e73c1fc8d1980f8a5c6f85733b2e
Shavrin Maksim <nutmegraw@gmail.com>
2022-02-15 16:03:20 +0500

    1) time limit for get_outputs added 2) just final moved from get_output() to __init__() 3) StayNeuron is deleted for useless

--- recurent.py
+++ recurent.py
@@ -1,18 +1,20 @@
-from neuron import ContinueNeuron, ControlCouple, StayNeuron, StopNeuron
+from time import time
+
+from neuron import ContinueNeuron, ControlCouple, StopNeuron
 from perceptron import Perceptron
 
 
 class RecurentPerceptron(Perceptron):
-    def __init__(self, structure, control_couples_number=1):
+    def __init__(self, structure, control_couples_number=1, just_final=False):
         super().__init__(structure)
         inputs_number = len(self.layers[-2].neurons)
-        self.essential_attrs.append(control_couples_number)
+        self.essential_attrs.extend([control_couples_number, just_final])
+        self.just_final = just_final
         self.control_couples = list()
         for _couple in range(control_couples_number):
             self.control_couples.append(ControlCouple(inputs_number))
-        self.stop_neuron = StayNeuron(inputs_number)
+        self.stop_neuron = StopNeuron(inputs_number)
         self.continue_neuron = ContinueNeuron(inputs_number)
-        self.stay_neuron = StayNeuron(inputs_number)
 
 
     def __repr__(self):
@@ -24,23 +26,25 @@ class RecurentPerceptron(Perceptron):
             index = round(((number + 1) / 2) * (self.weights_number - 1))
             self.all_weights[index].value = value
 
-    def get_outputs(self, inputs_values_list, just_final=False):
+    def get_outputs(self, inputs_values_list, time_limit=25):
+        start_time = time()
         resoults = list()
         for inputs_values in inputs_values_list:
-            is_stay = True
-            while is_stay:
-                resoult = super().get_outputs(inputs_values)
+            while True:
                 prelast_outputs = self._get_prelast_outputs(inputs_values)
-                self._change_weights(prelast_outputs)
+                resoult = super().get_outputs(inputs_values)
                 is_stop = self.stop_neuron.get_output(prelast_outputs)
                 is_continue = self.continue_neuron.get_output(prelast_outputs)
-                is_stay = self.stay_neuron.get_output(prelast_outputs)
+                self._change_weights(prelast_outputs)
                 if is_stop or is_continue:
                     break
-                if just_final:
+                if self.just_final:
                     resoults = resoult
                 else:
                     resoults.append(resoult)
+                if time() - start_time > time_limit:
+                    is_stop = True
+                    break
             if is_stop:
                 break
             if is_continue:
@@ -53,9 +57,10 @@ class RecurentPerceptron(Perceptron):
 
     @classmethod
     def init_from_weights(
-        cls, weights: list, structure: list, control_couples_number: int,
+        cls, weights: list, structure: list,
+        control_couples_number: int, just_final: bool,
     ):
-        new_perceptron = cls(structure, control_couples_number)
+        new_perceptron = cls(structure, control_couples_number, just_final)
         for position, weight in enumerate(new_perceptron.all_weights):
             weight.value = weights[position]
         return new_perceptron
@@ -70,5 +75,5 @@ class RecurentPerceptron(Perceptron):
     @property
     def all_neurons(self) -> list:
         return super().all_neurons + self.control_neurons + [
-            self.stop_neuron, self.continue_neuron, self.stay_neuron,
+            self.stop_neuron, self.continue_neuron,
         ]
