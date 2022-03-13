7cb5d18c5fc77ab9944ed86c46b8e1526b3111d0
Shavrin Maksim <nutmegraw@gmail.com>
2022-03-13 20:56:38 +0500

    1) jaro_winkler is cached 2) All is rewritten

--- recurent.py
+++ recurent.py
@@ -1,36 +1,40 @@
+from functools import lru_cache
 from time import time
 
+from numpy import mean
 from textdistance import jaro_winkler
 
 from neuron import ContinueNeuron, ControlCouple, StopNeuron
 from perceptron import Perceptron
-from utils import make_simple_structure
+
+
+jaro_winkler = lru_cache()(jaro_winkler)
 
 
 class RecurentPerceptron(Perceptron):
     def __init__(self, structure, control_couples_number=1):
         super().__init__(structure)
-        inputs_number = len(self.layers[-2].neurons)
         self.essential_attrs.append(control_couples_number)
         self.control_couples = list()
         for _couple in range(control_couples_number):
-            self.control_couples.append(ControlCouple(inputs_number))
+            self.control_couples.append(
+                ControlCouple(self.prelast_outputs_number),
+            )
 
-    def _change_weights(self, prelast_outputs):
+    def _change_weights(self):
         for couple in self.control_couples:
-            number, value = couple.get_outputs(prelast_outputs)
-            index = round(((number + 1) / 2) * (self.weights_number - 1))
+            number, value = couple.get_outputs(self.prelast_outputs)
+            index = round((number + 1) / 2 * (self.weights_number - 1))
             self.all_weights[index].value = value
 
-    def get_outputs(self, inputs_values_list, time_limit=25):
+    def get_outputs(self, inputs_values_list, time_limit=None):
         start_time = time()
         resoults = list()
         for inputs_values in inputs_values_list:
-            prelast_outputs = self._get_prelast_outputs(inputs_values)
             resoult = super().get_outputs(inputs_values)
-            self._change_weights(prelast_outputs)
+            self._change_weights()
             resoults.append(resoult)
-            if time() - start_time > time_limit:
+            if time_limit and time() - start_time > time_limit:
                 break
         return resoults
 
@@ -39,22 +43,11 @@ class RecurentPerceptron(Perceptron):
         for case_inputs, case_outputs in dataset:
             real_outputs = self.get_outputs(case_inputs)
             case_errors = list()
-            for case_output, real_output in list(
-                zip(case_outputs, real_outputs),
-            ):
+            for case_output, real_output in zip(case_outputs, real_outputs):
                 max_unit_error = self.max_unit_error(case_output, real_output)
                 case_errors.append(max_unit_error)
             cases_errors.append(max(case_errors))
         self.error = max(cases_errors)
-
-    @classmethod
-    def init_from_weights(
-        cls, weights: list, structure: list, control_couples_number: int,
-    ):
-        new_perceptron = cls(structure, control_couples_number)
-        for position, weight in enumerate(new_perceptron.all_weights):
-            weight.value = weights[position]
-        return new_perceptron
 
     @property
     def control_neurons(self) -> list:
@@ -71,61 +64,35 @@ class RecurentPerceptron(Perceptron):
 class NonComplimentalRecurent(RecurentPerceptron):
     def __init__(self, structure, control_couples_number=1):
         super().__init__(structure, control_couples_number)
-        inputs_number = len(self.layers[-2].neurons)
+        inputs_number = self.prelast_outputs_number
         self.stop_neuron = StopNeuron(inputs_number)
         self.continue_neuron = ContinueNeuron(inputs_number)
 
-    def get_outputs(self, inputs_values_list, time_limit=25):
+    def get_outputs(self, inputs_values_list, time_limit=None):
         start_time = time()
-        resoults = list()
-        for inputs_values in inputs_values_list:
-            while True:
-                prelast_outputs = self._get_prelast_outputs(inputs_values)
-                resoult = Perceptron.get_outputs(self, inputs_values)
-                is_stop = self.stop_neuron.get_output(prelast_outputs)
-                is_continue = self.continue_neuron.get_output(prelast_outputs)
-                self._change_weights(prelast_outputs)
-                if is_stop or is_continue:
+        is_stop = False
+        while not is_stop:
+            resoults = list()
+            for inputs_values in inputs_values_list:
+                while not is_stop:
+                    resoult = Perceptron.get_outputs(self, inputs_values)
+                    is_stop = self.stop_neuron\
+                        .get_output(self.prelast_outputs)
+                    is_continue = self.continue_neuron\
+                        .get_output(self.prelast_outputs)
+                    self._change_weights()
+                    if time_limit and ((time() - start_time) > time_limit):
+                        is_stop = True
+                    if is_stop or is_continue:
+                        break
+                    resoults.append(resoult)
+                if is_stop:
                     break
-                resoults.append(resoult)
-                if time() - start_time > time_limit:
-                    is_stop = True
-                    break
-            if is_stop:
-                break
-            if is_continue:
-                continue
+            if resoults:
+                inputs_values_list = resoults
+            else:
+                is_stop = True
         return resoults
-
-    @staticmethod
-    def _string_to_numbers_list(string: str) -> list:
-        return [ord(character) for character in string]
-
-    @staticmethod
-    def _numbers_list_to_string(numbers_list: list, unicode_max=1279) -> str:
-        chars_list = list()
-        for number in numbers_list:
-            chars_list.append(chr(round((number + 1) / 2 * unicode_max)))
-        return str().join(chars_list)
-
-    def get_answer(self, request: str) -> str:
-        numbers_list = self._string_to_numbers_list(request)
-        inputs_list = [[number] for number in numbers_list]
-        outputs_list = self.get_outputs(inputs_list)
-        numbers_list = [number.pop() for number in outputs_list]
-        return self._numbers_list_to_string(numbers_list)
-
-    def count_error(self, dataset) -> float:
-        cases_errors = list()
-        for case_inputs, case_outputs in dataset:
-            real_outputs = self.get_outputs(case_inputs)
-            cases_errors.append(
-                jaro_winkler(
-                    self._numbers_list_to_string(case_outputs),
-                    self._numbers_list_to_string(real_outputs),
-                ),
-            )
-        self.error = max(cases_errors)
 
     @property
     def all_neurons(self) -> list:
@@ -133,7 +100,7 @@ class NonComplimentalRecurent(RecurentPerceptron):
 
 
 class JustFinalRecurent(NonComplimentalRecurent):
-    def get_outputs(self, inputs_values_list, time_limit=25):
+    def get_outputs(self, inputs_values_list, time_limit=None):
         return super().get_outputs(inputs_values_list, time_limit).pop()
 
     def count_error(self, dataset) -> float:
@@ -142,24 +109,31 @@ class JustFinalRecurent(NonComplimentalRecurent):
             real_outputs = self.get_outputs(case_inputs)
             max_unit_error = self.max_unit_error(case_outputs, real_outputs)
             cases_errors.append(max_unit_error)
-        self.error = (cases_errors)
+        self.error = max(cases_errors)
 
 
+class LevelsRecurent(NonComplimentalRecurent):
+    def __init__(self, structure, control_couples_number=1, levels_number=512):
+        super().__init__(structure, control_couples_number)
+        self.essential_attrs.append(levels_number)
+        self.levels_number = levels_number
 
+    def get_outputs(self, inputs_values_list, time_limit=None) -> str:
+        raw_outputs = super().get_outputs(inputs_values_list, time_limit)
+        numbers_list = [number.pop() for number in raw_outputs]
+        return self._numbers_to_level_chars_string(numbers_list)
 
-if __name__ == '__main__':
-    def chat_with_bot():
-        chat_bot = NonComplimentalRecurent(
-            structure=make_simple_structure(
-                inputs_number=1,
-                intermediate_layers_number=7,
-                intermediate_layers_neurons_number=100,
-                outputs_number=1,
-            ),
-            control_couples_number=50,
-        )
-        while True:
-            answer = chat_bot.get_answer(input('You: '))
-            print(f'\nBot: {answer}\n')
+    def _numbers_to_level_chars_string(self, numbers_list: list) -> str:
+        chars_list = list()
+        for number in numbers_list:
+            chars_list.append(
+                chr(round((number + 1) / 2 * self.levels_number)),
+            )
+        return str().join(chars_list)
 
-    chat_with_bot()
+    def count_error(self, dataset, time_limit=None) -> float:
+        cases_errors = list()
+        for case_inputs, waited_string in dataset:
+            real_string = self.get_outputs(case_inputs, time_limit)
+            cases_errors.append(1 - jaro_winkler(waited_string, real_string))
+        self.error = mean(cases_errors)
