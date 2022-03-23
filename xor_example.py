ba6861d29315b4ebe8e0a85ff6d73c480d729f67
Shavrin Maksim <nutmegraw@gmail.com>
2022-03-23 22:38:00 +0500

    population size is changed to 100 000

--- xor_example.py
+++ xor_example.py
@@ -21,8 +21,9 @@ dataset = (
     ),
 )
 
+
 if __name__ == '__main__':
     net = Perceptron.load_from_file('/home/user/Desktop/My_folder/xor0.15.ann')
     # net = Perceptron([2, 3, 1])
-    net = net.tich_by_genetic(dataset, size=100, error=0.19)
+    net = net.tich_by_genetic(dataset, size=100000, error=0.19)
     net.save_to_file('/home/user/Desktop/My_folder/xor0.15.ann')
