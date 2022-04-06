b7480b6973f89b40dfe65e18eeaaae501af1717d
Shavrin Maksim <nutmegraw@gmail.com>
2022-04-06 20:33:03 +0500

    count_error is decorated  by @measure_execution_time for time returning form_couples function changed to generator tich function splited to few another functions

--- population.py
+++ population.py
@@ -1,10 +1,9 @@
 from random import choice, uniform
-from time import time
 
 from more_itertools import sort_together
 from numpy import mean
 
-from utils import PickleMixin, time_lenght_str
+from utils import PickleMixin, measure_execution_time, time_lenght_str
 
 
 class Population(PickleMixin):
@@ -14,27 +13,32 @@ class Population(PickleMixin):
         self.neuronets = [neuronet_type(*attrs) for _item in range(size)]
         self.generations = int()
 
+
     def change_size_to(self, neuronets_number):
         if self.size > neuronets_number:
             self.neuronets = self.neuronets[:neuronets_number]
         elif self.size < neuronets_number:
             additional_neuronets = self.__class__(
-                size=self.size-neuronets_number,
-                neuronet=self.neuronets[0],
+                size = self.size - neuronets_number,
+                neuronet = self.neuronets[0],
             ).neuronets
             self.neuronets += additional_neuronets
+
 
     @property
     def size(self) -> int:
         return len(self.neuronets)
 
+
     @property
     def errors(self) -> list:
         return [neuronet.error for neuronet in self.neuronets]
 
+
     @property
     def best_neuronet(self) -> object:
         return self.neuronets[self.errors.index(min(self.errors))]
+
 
     @staticmethod
     def cross_over(first_neuronet, second_neuronet, mutability) -> object:
@@ -54,11 +58,14 @@ class Population(PickleMixin):
             child_raw_weights, first_neuronet.essential_attrs,
         )
 
+
     def sort_by_errors(self):
         self.neuronets = list(
             sort_together([self.errors, self.neuronets])[1],
         )
 
+
+    @measure_execution_time
     def count_errors(self, dataset, time_limit=None):
         neuronets = list()
         for neuronet in self.neuronets:
@@ -69,29 +76,25 @@ class Population(PickleMixin):
             print(f'\rprogress: {percent}%', end=' | ')
             neuronet.count_error(dataset, time_limit)
 
-    def form_couples(self, couples_number) -> list:
+
+    def form_couples(self, couples_number) -> tuple:
         first_part = self.neuronets[::2]
         second_part = self.neuronets[1::2]
         half_lenght = int(self.size // 2)
         fuull_passes_number = int(couples_number // half_lenght)
         last_couples_number = int(couples_number % half_lenght)
-        couples = list()
         number = 1
         for first_neuronet, second_neuronet in zip(first_part, second_part):
             if (not fuull_passes_number) and (number > last_couples_number):
                 break
             for _number in range(fuull_passes_number):
-                couples.append((first_neuronet, second_neuronet))
+                yield (first_neuronet, second_neuronet)
             if number <= last_couples_number:
-                couples.append((first_neuronet, second_neuronet))
+                yield (first_neuronet, second_neuronet)
             number += 1
-        return couples
 
-    def tich(
-        self, dataset: list, mortality=0.4, error=0.25, mutability=0.2,
-        time_limit=None, ann_path=None, save_population=False,
-    ) -> object:
 
+    def print_chart_top(self, dataset, error, time_limit):
         print(
             f'population size: {self.size}',
             f'dataset lenght: {len(dataset)}',
@@ -101,25 +104,43 @@ class Population(PickleMixin):
         )
         print(74 * '=')
 
-        while True:
-            start_time = time()
-            self.count_errors(dataset, time_limit)
-            finish_time = time()
 
-            print(f'\rgeneration: {self.generations}', end=' | ')
-            print(f'mean error: {mean(self.errors)}', end=' | ')
-            print(f'best error: {self.best_neuronet.error}', end=' | ')
-            time_lenght = time_lenght_str(start_time, finish_time)
-            print(f'counting time: {time_lenght}', end=' | ')
+    def print_chart_line(self, time: float):
+        print(f'\rgeneration: {self.generations} ', end=' | ')
+        print(f'mean error: {mean(self.errors)}', end=' | ')
+        print(f'best error: {self.best_neuronet.error}', end=' | ')
+        print(f'counting time: {time_lenght_str(time)}', end=' | ')
+
+
+    def save_with_population(self, ann_path, save_population: bool):
+        if save_population:
+            self.best_neuronet.population = self
+        elif hasattr(self.best_neuronet, 'population'):
+            del self.best_neuronet.population
+        self.best_neuronet.save_to_file(ann_path)
+        print('saved', end='')
+
+
+    def enter_new_line(self):
+        print('')
+
+
+    def tich(
+        self, dataset: list, mortality=0.4, error=0.25, mutability=0.2,
+        time_limit=None, ann_path=None, save_population=False,
+    ) -> object:
+
+        self.print_chart_top(dataset, error, time_limit)
+
+        while True:
+            time = self.count_errors(dataset, time_limit)
+
+            self.print_chart_line(time)
+
             if ann_path:
-                if save_population:
-                    self.best_neuronet.population = self
-                else:
-                    if hasattr(self.best_neuronet, 'population'):
-                        del self.best_neuronet.population
-                self.best_neuronet.save_to_file(ann_path)
-                print('saved', end=' | ')
-            print('')
+                self.save_with_population(ann_path, save_population)
+            
+            self.enter_new_line()
 
             if self.best_neuronet.error < error:
                 return self.best_neuronet
