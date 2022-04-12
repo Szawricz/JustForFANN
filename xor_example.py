47d8fc7e9a45d741eabc8335146764a6a2ecb027
Shavrin Maksim <nutmegraw@gmail.com>
2022-04-12 13:43:12 +0500

    rewritten a little bit

--- xor_example.py
+++ xor_example.py
@@ -1,5 +1,5 @@
 from perceptron import Perceptron
-from utils import make_simple_structure
+
 
 # XOR problem example:
 dataset = (
@@ -23,6 +23,8 @@ dataset = (
 
 
 if __name__ == '__main__':
-    net = Perceptron(structure=[2, 3, 1])
-    # net = Perceptron.load_from_file('/home/user/Desktop/My_folder/xor0.15.ann')
+    net = Perceptron([2, 3, 1])
+    # net = Perceptron.load_from_file(
+    #     '/home/user/Desktop/My_folder/xor0.15.ann',
+    # )
     net = net.tich_by_genetic(dataset, size=100000, error=0.19)
