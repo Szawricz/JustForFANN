f4e8f845154adcbbcde5ab679e4561ba9bb8877e
Shavrin Maksim <nutmegraw@gmail.com>
2022-04-12 13:35:48 +0500

    little codestyle fix

--- chatbot_example.py
+++ chatbot_example.py
@@ -1,14 +1,18 @@
 from generative import ChatBot
 from utils import make_simple_structure
 
+
 # chat_bot = ChatBot.load_from_file(
 #     '/home/user/Desktop/My_folder/brain.ann',
 # )
+
 
 chat_bot = ChatBot(
     make_simple_structure(1, 10, 100, 1),
     control_couples_number=10,
 )
+
+
 dataset = [
     ['Hey!', 'Hallo!'],
     ['0 + 0 = ?', '0'],
@@ -23,6 +27,7 @@ dataset = [
     ['What is cat?', 'Cat is such animal'],
     ['1 + 0 = ?', '1'],
 ]
+
 
 if __name__ == '__main__':
     chat_bot = chat_bot.tich_by_genetic(dataset, size=10, time_limit=60)
