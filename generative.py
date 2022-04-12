1bbc0040ed54effaa2aaaca0646ca0ba5f8fa15b
Shavrin Maksim <nutmegraw@gmail.com>
2022-04-12 13:41:21 +0500

    Some classes are moved to its own new module

---
+++ generative.py
@@ -0,0 +1,133 @@
+from functools import lru_cache
+from string import printable
+from time import time
+
+from numpy import mean
+from textdistance import jaro_winkler
+
+from neuron import BiasNeuron, ContinueNeuron, StopNeuron
+from perceptron import Perceptron
+from recurent import RecurentPerceptron
+
+jaro_winkler = lru_cache()(jaro_winkler)
+
+
+class NonComplimentalRecurent(RecurentPerceptron):
+    def __init__(self, structure, control_couples_number=1):
+        super().__init__(structure, control_couples_number)
+        inputs_number = self.prelast_outputs_number
+        self.stop_neuron = StopNeuron(inputs_number)
+        self.continue_neuron = ContinueNeuron(inputs_number)
+
+    def inverse_biases(self, value: bool):
+        for neuron in self.all_neurons:
+            if isinstance(neuron, BiasNeuron):
+                neuron.inversed = value
+
+    def get_outputs(
+        self, inputs_values_list, just_final=False, time_limit=None,
+    ):
+        self.inverse_biases(False)
+        start_time = time()
+        is_stop = False
+
+        while not is_stop:
+            resoults = list()
+            for inputs_values in inputs_values_list:
+                while not is_stop:
+                    resoult = Perceptron.get_outputs(self, inputs_values)
+                    is_stop = self.stop_neuron\
+                        .get_output(self.prelast_outputs)
+                    is_continue = self.continue_neuron\
+                        .get_output(self.prelast_outputs)
+                    self._change_weights()
+                    if time_limit and ((time() - start_time) > time_limit):
+                        is_stop = True
+                    if is_stop or is_continue:
+                        break
+                    if just_final:
+                        resoults = list().append(resoult)
+                    else:
+                        resoults.append(resoult)
+                if is_stop:
+                    break
+            if resoults:
+                inputs_values_list = resoults
+                self.inverse_biases(True)
+            else:
+                is_stop = True
+
+        self.inverse_biases(False)
+        if just_final:
+            return resoult
+        return resoults
+
+    @property
+    def all_neurons(self) -> list:
+        return super().all_neurons + [self.stop_neuron, self.continue_neuron]
+
+
+class JustFinalRecurent(NonComplimentalRecurent):
+    def get_outputs(self, inputs_values_list, time_limit=None):
+        return super().get_outputs(
+            inputs_values_list, time_limit, just_final=True,
+        )
+
+    def count_error(self, dataset) -> float:
+        cases_errors = list()
+        for case_inputs, case_outputs in dataset:
+            real_outputs = self.get_outputs(case_inputs)
+            max_unit_error = self.max_unit_error(case_outputs, real_outputs)
+            cases_errors.append(max_unit_error)
+        self.error = max(cases_errors)
+
+
+class LevelsRecurent(NonComplimentalRecurent):
+    def __init__(self, structure, control_couples_number=1, levels_number=512):
+        super().__init__(structure, control_couples_number)
+        self.essential_attrs.append(levels_number)
+        self.levels_number = levels_number
+
+    def get_outputs(self, inputs_values_list, time_limit=None) -> str:
+        raw_outputs = super().get_outputs(inputs_values_list, time_limit)
+        numbers_list = [number.pop() for number in raw_outputs]
+        return self._numbers_to_level_chars_string(numbers_list)
+
+    def _numbers_to_level_chars_string(self, numbers_list: list) -> str:
+        chars_list = list()
+        for number in numbers_list:
+            chars_list.append(
+                chr(round((number + 1) / 2 * self.levels_number)),
+            )
+        return str().join(chars_list)
+
+    def count_error(self, dataset, time_limit=None) -> float:
+        cases_errors = list()
+        for case_inputs, waited_string in dataset:
+            real_string = self.get_outputs(case_inputs, time_limit)
+            cases_errors.append(1 - jaro_winkler(waited_string, real_string))
+        self.error = mean(cases_errors)
+
+
+class ChatBot(LevelsRecurent):
+    def __init__(self, structure, control_couples_number=1, charset=printable):
+        super().__init__(structure, control_couples_number)
+        self.levels_number = len(charset)
+        self.essential_attrs[-1] = charset
+        self.charset = charset
+
+    @staticmethod
+    def _string_to_numbers_list(string: str) -> list:
+        return [ord(character) for character in string]
+
+    def _numbers_to_level_chars_string(self, numbers_list: list) -> str:
+        chars_list = list()
+        for number in numbers_list:
+            index = (round((number + 1) / 2) * (self.levels_number - 1))
+            chars_list.append(self.charset[index])
+        return str().join(chars_list)
+
+    def get_outputs(self, input_string, time_limit=None) -> str:
+        numbers_list = self._string_to_numbers_list(input_string)
+        inputs_list = [[number] for number in numbers_list]
+        return super().get_outputs(inputs_list, time_limit)
--- recurent.py
+++ recurent.py
@@ -1,14 +1,7 @@
-from functools import lru_cache
 from time import time
 
-from numpy import mean
-from textdistance import jaro_winkler
-
-from neuron import ContinueNeuron, ControlCouple, StopNeuron, BiasNeuron
+from neuron import ControlCouple
 from perceptron import Perceptron
-
-
-jaro_winkler = lru_cache()(jaro_winkler)
 
 
 class RecurentPerceptron(Perceptron):
@@ -59,100 +52,3 @@ class RecurentPerceptron(Perceptron):
     @property
     def all_neurons(self) -> list:
         return super().all_neurons + self.control_neurons
-
-
-class NonComplimentalRecurent(RecurentPerceptron):
-    def __init__(self, structure, control_couples_number=1):
-        super().__init__(structure, control_couples_number)
-        inputs_number = self.prelast_outputs_number
-        self.stop_neuron = StopNeuron(inputs_number)
-        self.continue_neuron = ContinueNeuron(inputs_number)
-
-    def inverse_biases(self, value: bool):
-        for neuron in self.all_neurons:
-            if isinstance(neuron, BiasNeuron):
-                neuron.inversed = value
-
-    def get_outputs(
-        self, inputs_values_list, just_final=False, time_limit=None,
-    ):
-        self.inverse_biases(False)
-        start_time = time()
-        is_stop = False
-
-        while not is_stop:
-            resoults = list()
-            for inputs_values in inputs_values_list:
-                while not is_stop:
-                    resoult = Perceptron.get_outputs(self, inputs_values)
-                    is_stop = self.stop_neuron\
-                        .get_output(self.prelast_outputs)
-                    is_continue = self.continue_neuron\
-                        .get_output(self.prelast_outputs)
-                    self._change_weights()
-                    if time_limit and ((time() - start_time) > time_limit):
-                        is_stop = True
-                    if is_stop or is_continue:
-                        break
-                    if just_final:
-                        resoults = list().append(resoult)
-                    else:
-                        resoults.append(resoult)
-                if is_stop:
-                    break
-            if resoults:
-                inputs_values_list = resoults
-                self.inverse_biases(True)
-            else:
-                is_stop = True
-
-        self.inverse_biases(False)
-        if just_final:
-            return resoult
-        return resoults
-
-    @property
-    def all_neurons(self) -> list:
-        return super().all_neurons + [self.stop_neuron, self.continue_neuron]
-
-
-class JustFinalRecurent(NonComplimentalRecurent):
-    def get_outputs(self, inputs_values_list, time_limit=None):
-        return super().get_outputs(
-            inputs_values_list, time_limit, just_final=True,
-        )
-
-    def count_error(self, dataset) -> float:
-        cases_errors = list()
-        for case_inputs, case_outputs in dataset:
-            real_outputs = self.get_outputs(case_inputs)
-            max_unit_error = self.max_unit_error(case_outputs, real_outputs)
-            cases_errors.append(max_unit_error)
-        self.error = max(cases_errors)
-
-
-class LevelsRecurent(NonComplimentalRecurent):
-    def __init__(self, structure, control_couples_number=1, levels_number=512):
-        super().__init__(structure, control_couples_number)
-        self.essential_attrs.append(levels_number)
-        self.levels_number = levels_number
-
-    def get_outputs(self, inputs_values_list, time_limit=None) -> str:
-        raw_outputs = super().get_outputs(inputs_values_list, time_limit)
-        numbers_list = [number.pop() for number in raw_outputs]
-        return self._numbers_to_level_chars_string(numbers_list)
-
-    def _numbers_to_level_chars_string(self, numbers_list: list) -> str:
-        chars_list = list()
-        for number in numbers_list:
-            chars_list.append(
-                chr(round((number + 1) / 2 * self.levels_number)),
-            )
-        return str().join(chars_list)
-
-    def count_error(self, dataset, time_limit=None) -> float:
-        cases_errors = list()
-        for case_inputs, waited_string in dataset:
-            real_string = self.get_outputs(case_inputs, time_limit)
-            cases_errors.append(1 - jaro_winkler(waited_string, real_string))
-        self.error = mean(cases_errors)
