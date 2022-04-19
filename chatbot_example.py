3f12d4a42c4df2581f70b4080c457c69450e0498
Shavrin Maksim <nutmegraw@gmail.com>
2022-04-19 21:56:09 +0500

    little fixes

--- chatbot_example.py
+++ chatbot_example.py
@@ -4,25 +4,18 @@ from generative import ChatBot
 from utils import make_simple_structure
 
 
-# chat_bot = ChatBot.load_from_pickle(
-#     '/home/user/Desktop/My_folder/brain.ann',
-# )
-
-
 chat_bot = ChatBot(
     make_simple_structure(1, 10, 100, 1),
     control_couples_number=10,
 )
 
+chat_bot = ChatBot.load_from_pickle('/home/user/Desktop/My_folder/brain.ann')
 
 dataset = [
     ['Hey!', 'Hallo!'],
     ['0 + 0 = ?', '0'],
     ['0 + 1 = ?', '1'],
-    [
-        'Say the letters of English alphabet, please.',
-        ascii_uppercase,
-    ],
+    ['Say the letters of English alphabet, please.', ascii_uppercase],
     ['Could you answer nothing?', ''],
     ['Repeat please word "cat"', 'cat. What is cat?'],
     ['Cat is such animal', 'Thanks.'],
