67a227aa89a059368dfaf5c94cabdf9868c52a34
Shavrin Maksim <nutmegraw@gmail.com>
2022-04-25 19:46:55 +0500

    last_layer_neurons_number renamed to neuron_inputs_number

--- layer.py
+++ layer.py
@@ -3,39 +3,34 @@ from neuron import BiasNeuron, InputNeuron, InternalLayerNeuron, OutputNeuron
 
 class AbstractLayer:
     def __init__(
-        self, last_layer_neurons_number: int,
-        neurons_number: int, neuron_type, with_bias=True,
+        self, neuron_inputs_number: int, neurons_number: int,
+        neuron_type, with_bias=True,
     ):
+        self.neurons = list()
         if with_bias:
-            start_neurons = [BiasNeuron()]
-        else:
-            start_neurons = list()
-        self.neurons = start_neurons
-        for _neuron_number in range(neurons_number - with_bias):
-            neuron = neuron_type(inputs_number=last_layer_neurons_number)
-            self.neurons.append(neuron)
+            self.neurons.append(BiasNeuron())
+        for _neuron_number in range(neurons_number):
+            self.neurons.append(neuron_type(neuron_inputs_number)) 
 
     def get_outputs(self, inputs_values: list) -> list:
         return [neuron.get_output(inputs_values) for neuron in self.neurons]
 
 
 class InputLayer(AbstractLayer):
-    def __init__(self, last_layer_neurons_number: int, neurons_number: int):
-        super().__init__(
-            last_layer_neurons_number, neurons_number, InputNeuron,
-        )
+    def __init__(self, neuron_inputs_number: int, neurons_number: int):
+        super().__init__(neuron_inputs_number, neurons_number, InputNeuron)
 
 
 class InternalLayer(AbstractLayer):
-    def __init__(self, last_layer_neurons_number: int, neurons_number: int):
+    def __init__(self, neuron_inputs_number: int, neurons_number: int):
         super().__init__(
-            last_layer_neurons_number, neurons_number, InternalLayerNeuron,
+            neuron_inputs_number, neurons_number, InternalLayerNeuron,
         )
 
 
 class OutputLayer(AbstractLayer):
-    def __init__(self, last_layer_neurons_number: int, neurons_number: int):
+    def __init__(self, neuron_inputs_number: int, neurons_number: int):
         super().__init__(
-            last_layer_neurons_number, neurons_number, OutputNeuron,
+            neuron_inputs_number, neurons_number, OutputNeuron,
             with_bias=False,
         )
