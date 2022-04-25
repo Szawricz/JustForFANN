b13ff4dee07fb32b94b25a58f677239843d1d362
Shavrin Maksim <nutmegraw@gmail.com>
2022-04-25 20:58:21 +0500

    1) pickling decorator is used 2) init method is rewritten with neuronet_example using 3) neuronet_example for the whole module 4)sorting by error is relocated to error counting function 5)rjust and ljust are used to make chartline better

--- population.py
+++ population.py
@@ -4,60 +4,74 @@ from time import time
 from more_itertools import sort_together
 from numpy import mean
 
-from utils import (pickling, print_percent, print_spases_line, time_lenght_str,
+from utils import (print_percent, print_spases_line, time_lenght_str,
                    with_current_process_print,
                    with_start_and_finish_time_print)
 
 
-@pickling
 class Population:
     def __init__(self, size: int, neuronet: object):
-        neuronet_type = neuronet.__class__
-        attrs = neuronet.essential_attrs
-        self.neuronets = [neuronet_type(*attrs) for _item in range(size)]
-        self.generations = int()
+        self.neuronet_example = neuronet
+
+        self.neuronets = list()
+        for _item in range(size):
+            neuronet = self.neuronet_example.copy_with_random_weights()
+            self.neuronets.append(neuronet)
+
+        self.generations = int(0)
 
     @with_start_and_finish_time_print
     def tich(
         self, dataset: list, mortality=0.4, error=0.25, mutability=0.2,
         time_limit=None, ann_path=None, save_population=False,
     ) -> object:
-
+        # print the popuilation tichg parameters in chart top
         self._print_chart_top(dataset, error, time_limit)
+
+        # start the time measuring for the ancestors population
         start_time = time()
 
         while True:
+            # count errors and sort neuronets by the errors
             self._count_errors(dataset, time_limit)
 
+            # save tiched neuronet to file
             if ann_path:
                 self._save_best_neuronet(ann_path, save_population)
 
+            # time counting finishing
             finish_time = time()
-            resoult_time = finish_time - start_time
-            start_time = time()
 
+            # output resoults of generation's tiching to console
+            resoult_time = finish_time - start_time
             self._print_chart_line(resoult_time, ann_path, save_population)
 
+            # stop the tiching and return resouult if goal arror reached
             if self.best_neuronet.error < error:
                 return self.best_neuronet
 
-            self._sort_by_errors()
+            # time counting starting
+            start_time = time()
 
+            # Killing worst neuronets
             dead_nets_number = round(mortality * self.size)
             survived_nets_number = self.size - dead_nets_number
-
             self.change_size_to(survived_nets_number)
+
+            # breeding
             couples = self._form_couples(dead_nets_number)
             children = self._make_children(couples, mutability)
-            self.generations += 1
+
+            # children adding to population
             self.neuronets += children
+            self.generations += 1
 
     def change_size_to(self, neuronets_number):
         if self.size > neuronets_number:
             self.neuronets = self.neuronets[:neuronets_number]
         elif self.size < neuronets_number:
             size = neuronets_number - self.size
-            neuronet = self.neuronets[0]
+            neuronet = self.neuronet_example
             additional_neuronets = self.__class__(size, neuronet).neuronets
             self.neuronets.extend(additional_neuronets)
 
@@ -67,7 +81,7 @@ class Population:
 
     @property
     def best_neuronet(self) -> object:
-        return self.neuronets[self._errors.index(min(self._errors))]
+        return self.neuronets[0]
 
     @property
     def _errors(self) -> list:
@@ -81,26 +95,26 @@ class Population:
         )
         child_raw_weights = list()
         for first_weight, second_weight in compliment_weights_couples:
-            if choice([True, False]):
-                child_raw_weight = first_weight.value
-            else:
-                child_raw_weight = second_weight.value
+            child_raw_weight = choice([first_weight, second_weight]).value
             if uniform(0, 1) < mutability:
                 child_raw_weight = first_weight.value_generator()
             child_raw_weights.append(child_raw_weight)
-        return first_neuronet.__class__.init_from_weights(
-            child_raw_weights, first_neuronet.essential_attrs,
-        )
+        return self.neuronet_example.copy_with_new_weights(child_raw_weights)
 
     @with_current_process_print('saving...')
     def _save_best_neuronet(self, ann_path: str, with_population: bool):
+        # bound population to the best neuronet will be saved to file
         if with_population:
             self.best_neuronet.population = self
-        self.best_neuronet.save_to_pickle(ann_path)
+
+        # save neuronet by "save_to_file" method provided by pickling mixin
+        self.best_neuronet.save_to_file(ann_path)
+
+        # delete the binded population to prevent a recursion 
         if with_population:
             del self.best_neuronet.population
 
-    def _make_children(self, couples: list, mutability: float):
+    def _make_children(self, couples: list, mutability: float) -> list:
         children = list()
         couples = list(couples)
         for number, couple in enumerate(couples, start=1):
@@ -110,9 +124,7 @@ class Population:
         return children
 
     def _sort_by_errors(self):
-        self.neuronets = list(
-            sort_together([self._errors, self.neuronets])[1],
-        )
+        self.neuronets = list(sort_together([self._errors, self.neuronets])[1])
 
     @property
     def _neuronets_without_counted_error(self):
@@ -125,19 +137,28 @@ class Population:
         return neuronets
 
     def _count_errors(self, dataset, time_limit=None):
-        neuronets = self._neuronets_without_counted_error
+        if self.neuronet_example.recurent:
+            neuronets = self.neuronets
+        else:
+            neuronets = self._neuronets_without_counted_error
+
         for number, neuronet in enumerate(neuronets, start=1):
             print_percent('errors counting:', number, neuronets)
             neuronet.count_error(dataset, time_limit)
-        print_spases_line()
+            print_spases_line()
+
+        # sort all population neuronrts after its errors counting
+        self._sort_by_errors()
 
     @with_current_process_print('forming...')
     def _form_couples(self, couples_number) -> tuple:
         first_part = self.neuronets[::2]
         second_part = self.neuronets[1::2]
+
         half_lenght = int(self.size // 2)
         fuull_passes_number = int(couples_number // half_lenght)
         last_couples_number = int(couples_number % half_lenght)
+
         number = 1
         for first_neuronet, second_neuronet in zip(first_part, second_part):
             if (not fuull_passes_number) and (number > last_couples_number):
@@ -150,7 +171,7 @@ class Population:
 
     def _print_chart_top(self, dataset, error, time_limit):
         print(
-            f'population size: {self.size}',
+            f'| population size: {self.size}',
             f'dataset lenght: {len(dataset)}',
             f'goal error: {error}',
             f'time limit per case: {time_limit} sec',
@@ -160,10 +181,15 @@ class Population:
 
     def _print_chart_line(
         self, time: float, ann_path: str, save_population: bool,
+        errors_lenght=19, gen_number_lenght=4,
     ):
-        print(f'\rgeneration: {self.generations}', end=' | ')
-        print(f'mean error: {mean(self._errors)}', end=' | ')
-        print(f'best error: {self.best_neuronet.error}', end=' | ')
+        best_error = str(self.best_neuronet.error).ljust(errors_lenght, '0')
+        mean_error = str(mean(self._errors)).ljust(errors_lenght, '0')
+        generation_number = str(self.generations).rjust(gen_number_lenght)
+
+        print(f'\rgeneration: {generation_number}', end=' | ')
+        print(f'mean error: {mean_error}', end=' | ')
+        print(f'best error: {best_error}', end=' | ')
         print(f'counting time: {time_lenght_str(time)}', end=' | ')
         if ann_path:
             print('saved', end=' ')
