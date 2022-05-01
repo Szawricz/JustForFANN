143d9c4b10e447dab663dc12ba49d4288c039f05
Shavrin Maksim <nutmegraw@gmail.com>
2022-05-01 18:31:22 +0500

    functions order is changed

--- population.py
+++ population.py
@@ -90,28 +90,6 @@ class Population:
     def _errors(self) -> list:
         return [neuronet.error for neuronet in self.neuronets]
 
-    def _cross_over(self, neuronet_1, neuronet_2, mutability: float):
-        child_raw_weights = list()
-        for couple in zip(neuronet_1.all_weights, neuronet_2.all_weights):
-            child_raw_weight = choice(couple).value
-            if uniform(0, 1) < mutability:
-                child_raw_weight = couple[0].value_generator()
-            child_raw_weights.append(child_raw_weight)
-        return self.neuronet_example.copy_with_new_weights(child_raw_weights)
-
-    @with_current_process_print('saving...')
-    def _save_best_neuronet(self, ann_path: str, with_population: bool):
-        # bound population to the best neuronet will be saved to file
-        if with_population:
-            self.best_neuronet.population = self
-
-        # save neuronet by "save_to_file" method provided by pickling mixin
-        self.best_neuronet.save_to_file(ann_path)
-
-        # delete the binded population to prevent a recursion 
-        if with_population:
-            del self.best_neuronet.population
-
     def _make_children(self, couples: list, mutability: float) -> list:
         children = list()
         couples = list(couples)
@@ -121,24 +99,62 @@ class Population:
         print_spases_line()
         return children
 
+    def _cross_over(self, neuronet_1, neuronet_2, mutability: float):
+        child_raw_weights = list()
+        for couple in zip(neuronet_1.all_weights, neuronet_2.all_weights):
+            child_raw_weight = choice(couple).value
+            if uniform(0, 1) < mutability:
+                child_raw_weight = couple[0].value_generator()
+            child_raw_weights.append(child_raw_weight)
+        return self.neuronet_example.copy_with_new_weights(child_raw_weights)
+
+    @with_current_process_print('forming couples...')
+    def _form_couples(self, couples_number) -> tuple:
+        parts = split_by_evenodd_position(self.neuronets)
+
+        half_lenght = int(self.size // 2)
+
+        fuull_passes_number = int(couples_number // half_lenght)
+        last_couples_number = int(couples_number % half_lenght)
+
+        for number, couple in enumerate(zip(*parts), start=1):
+            if (not fuull_passes_number) and (number > last_couples_number):
+                break
+            for _number in range(fuull_passes_number):
+                yield couple
+            if number <= last_couples_number:
+                yield couple
+
+    @with_current_process_print('saving best neuronet...')
+    def _save_best_neuronet(self, ann_path: str, with_population: bool):
+        # bound population to the best neuronet will be saved to file
+        if with_population:
+            self.best_neuronet.population = self
+
+        # save neuronet by "save_to_file" method provided by pickling mixin
+        self.best_neuronet.save_to_file(ann_path)
+
+        # delete the binded population to prevent a recursion
+        if with_population:
+            del self.best_neuronet.population
+
     def _sort_by_errors(self):
         self.neuronets = list(sort_together([self._errors, self.neuronets])[1])
 
     @property
-    def _neuronets_without_counted_error(self):
-        neuronets = list()
+    def _neuronets_without_error(self) -> list:
+        without_error = list()
         for number, neuronet in enumerate(self.neuronets, start=1):
             print_percent('neuronets preparing:', number, self.neuronets)
             if not neuronet.error:
-                neuronets.append(neuronet)
+                without_error.append(neuronet)
         print_spases_line()
-        return neuronets
+        return without_error
 
     def _count_errors(self, dataset, time_limit=None):
-        if self.neuronet_example.recurent:
-            neuronets = self.neuronets
-        else:
-            neuronets = self._neuronets_without_counted_error
+        neuronets = self.neuronets
+        if not self.neuronet_example.recurent:
+            neuronets = self._neuronets_without_error
 
         for number, neuronet in enumerate(neuronets, start=1):
             print_percent('errors counting:', number, neuronets)
@@ -147,22 +163,6 @@ class Population:
 
         # sort all population neuronrts after its errors counting
         self._sort_by_errors()
-
-    @with_current_process_print('forming...')
-    def _form_couples(self, couples_number) -> tuple:
-        parts = split_by_evenodd_position(self.neuronets)
-
-        half_lenght = int(self.size // 2)
-        fuull_passes_number = int(couples_number // half_lenght)
-        last_couples_number = int(couples_number % half_lenght)
-
-        for number, couple in enumerate(zip(parts), start=1):
-            if (not fuull_passes_number) and (number > last_couples_number):
-                break
-            for _number in range(fuull_passes_number):
-                yield couple
-            if number <= last_couples_number:
-                yield couple
 
     def _print_chart_top(self, dataset, error, time_limit):
         print(
