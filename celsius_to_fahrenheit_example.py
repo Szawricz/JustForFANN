66a6503031f64ba44b8e72dccfeb78b01cdc22ab
Shavrin Maksim <nutmegraw@gmail.com>
2022-05-17 20:03:05 +0500

    scaler for input data is added

--- celsius_to_fahrenheit_example.py
+++ celsius_to_fahrenheit_example.py
@@ -4,36 +4,52 @@ from perceptron import Perceptron
 from utils import make_simple_structure, Scaler
 
 
+celsius_scaler = Scaler(min_value=-273, max_value=1000)
+
+
 def celsius_to_fahrenheit(celsius: float) -> float:
     return 1.8 * celsius + 32
 
 
-def generate_dataset(items_number: int) -> list:
-    dataset = list()
-    for _number in range(items_number):
-        celsius = uniform(-273, 1000)
-        fahrenheit = celsius_to_fahrenheit(celsius)
-        waited_output_value = Scaler(
+fahrenheit_scaler = Scaler(
             min_value=celsius_to_fahrenheit(-273),
             max_value=celsius_to_fahrenheit(1000),
-        ).to_neuronet_format(fahrenheit)
+        )
+
+
+def generate_dataset() -> list:
+    dataset = list()
+    for celsius in range(-273, 1000):
+        fahrenheit = celsius_to_fahrenheit(celsius)
+        input_value = celsius_scaler.to_neuronet_format(celsius)
+        waited_output_value = fahrenheit_scaler.to_neuronet_format(fahrenheit)
         dataset.append(
             (
-                (celsius,),
+                (input_value,),
                 (waited_output_value,),
             ),
         )
     return dataset
 
 
-net = Perceptron(make_simple_structure(1, 6, 10, 1))
+if __name__ == '__main__':
+    net = Perceptron(make_simple_structure(1, 2, 100, 1))
 
-net = Perceptron.load_from_file(
-    '/home/user/Desktop/My_folder/celsius_to_fahrenheit.ann',
-)
+    net = Perceptron.load_from_file(
+        '/home/user/Desktop/My_folder/celsius_to_fahrenheit.ann',
+    )
 
-net.tich_by_genetic(
-    size=1000,
-    dataset=generate_dataset(10000),
-    ann_path='/home/user/Desktop/My_folder/celsius_to_fahrenheit.ann',
-)
+    net.tich_by_genetic(
+        size=100,
+        dataset=generate_dataset(),
+        ann_path='/home/user/Desktop/My_folder/celsius_to_fahrenheit.ann',
+    )
+
+    while True:
+        inputs = celsius_scaler\
+            .to_neuronet_format(float(input(prompt='celsius: ')))
+        inputs = [inputs]
+        print(
+            'fahrenheit:',
+            fahrenheit_scaler.from_neuronet_format(net.get_outputs(inputs)[0]),
+        )
